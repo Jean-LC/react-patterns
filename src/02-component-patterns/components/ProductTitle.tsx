481062d68f152e3f835fa92f0aaf91d0244bb0c5
Jeanette Latisnere <JLatisnere@LICON.COM.MX>
2023-04-25 08:25:45 -0600

    finishes component compund

---
+++ src/02-component-patterns/components/ProductTitle.tsx
@@ -0,0 +1,15 @@
+import { ProductContext } from "./ProductCard";
+import { useContext } from "react";
+import styles from "../styles/styles.module.css";
+
+export const ProductTitle = ({ title }: { title?: string }) => {
+  const { product } = useContext(ProductContext);
+  let titleToShow: string;
+
+  if (title) {
+    titleToShow = title;
+  } else {
+    titleToShow = product.title;
+  }
+  return <span className={styles.productDescription}>{titleToShow}</span>;
+};
--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,4 +1,9 @@
-import { ProductCard } from "../components/ProductCard";
+import {
+  ProductButtons,
+  ProductCard,
+  ProductImage,
+  ProductTitle,
+} from "../components";
 
 const product = {
   id: "1",
@@ -18,7 +23,11 @@ export const ShoppingPage = () => {
           flexWrap: "wrap",
         }}
       >
-        <ProductCard product={product} />
+        <ProductCard product={product}>
+          <ProductImage />
+          <ProductTitle />
+          <ProductButtons />
+        </ProductCard>
       </div>
     </div>
   );
